8fbf5db183284f6db8fd9b966096884ca4fd4ae5
research-scientist-online <8367366+ResearchScientist@users.noreply.github.com>
2021-01-27 21:46:01 +0000

    issue: functions not found

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@ module.exports = (config) => {
     config.addPassthroughCopy('src/css');
     config.addPassthroughCopy('src/js');
     config.addPassthroughCopy('src/images');
-}
+    return {
+    dir: {
+        input: 'src',
+    },
+  };
+};
