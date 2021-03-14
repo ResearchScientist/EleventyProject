e29a0726034e7da8f8bde06c8ecfe378678d2a0f
research-scientist-online <8367366+ResearchScientist@users.noreply.github.com>
2021-03-14 22:13:13 +0000

    refactor code

--- functions/movies.js
+++ functions/movies.js
@@ -2,7 +2,6 @@ const movies = require('../data/movies.json');
 
 exports.handler = async () => {
     return {
-        console.log('movies json found');
         statusCode: 200,
         body: JSON.stringify(movies),
     };
